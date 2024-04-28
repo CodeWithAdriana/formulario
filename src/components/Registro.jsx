bc810c338a607caf428b09a45654dbd1fa2e5b69
Adri <adrianareyesgarcia1@gmail.com>
2024-04-27 19:42:05 -0500

    1Commit SocialButton

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -1,27 +1,17 @@
-import { useState } from "react"
-import Formulario from "./Formulario"
+import { useState } from "react";
+import Formulario from "./Formulario";
+import SocialButton from "./SocialButton";
 
-function Registro(){
-    const [alert, setAlert]= useState({mensaje:"", color:""})
+function Registro() {
+  const [alert, setAlert] = useState({ mensaje: "", color: "" });
 
-    return(
-        <>
-        <h3>Completa el Formulario</h3>
-        <Formulario setAlert={setAlert}
-        />
-        {alert.mensaje && <h1 style={{color:alert.color}}>{alert. mensaje}</h1> }
-        </>
-    )
-
+  return (
+    <>
+      <h3>Completa el Formulario</h3>
+      <Formulario setAlert={setAlert} />
+      {alert.mensaje && <h1 style={{ color: alert.color }}>{alert.mensaje}</h1>}
+    </>
+  );
 }
 
-export default Registro
-
-
-
-
-
-
-
-
-    
+export default Registro;
---
+++ src/components/SocialButton.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const SocialButton = ({ icon, onClick }) => {
+  return <button onCLick={onClick} className=""></button>;
+};
